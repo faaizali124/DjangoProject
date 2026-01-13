152862969e03c8eb58bb2c2aa9ff81949246c4ef
faaizali124 <faaizali124@gmail.com>
2026-01-13 21:48:47 +0500

    added a model for Posts and displayed the Posts on the home screen

--- main/views.py
+++ main/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 
 def home_screen(request):
-    return render(request, 'home.html')
+    posts = Post.objects.all()
+    return render(request, 'home.html', {'posts':posts})
 
 def about(request):
     return render(request, 'about.html')
