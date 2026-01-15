6381580e2ae9dc1ca61c3885027283cbed23537e
faaizali124 <faaizali124@gmail.com>
2026-01-15 22:13:20 +0500

    Added functionality for users to create their own posts on the front-end using forms.

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 #URLConfiguration
-urlpatterns = [path('', views.home_screen),
-               path('about/', views.about),
-               path('contact/', views.contact),
-               path('post/<int:post_id>/', views.post_detail, name = 'post_detail')]
+urlpatterns = [path('', views.home_screen, name = 'home'),
+               path('about/', views.about, name = 'about'),
+               path('contact/', views.contact, name = 'contact'),
+               path('post/<int:post_id>/', views.post_detail, name = 'post_detail'),
+               path('add-post/', views.add_post, name = 'add_post')]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.http import HttpResponse
 from .models import Post
+from .forms import PostForm
 
 def home_screen(request):
     posts = Post.objects.all()
@@ -15,3 +16,16 @@ def contact(request):
 def post_detail(request, post_id):
     post = get_object_or_404(Post, id=post_id)
     return render(request, 'post_detail.html', {'post': post})
+
+def add_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = PostForm()
+    
+    return render(request, 'add_post.html', {'form':form})
+
+    
